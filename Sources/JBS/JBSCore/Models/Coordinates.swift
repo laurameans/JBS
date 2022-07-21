f4478cf8022205b50faf9e32e208472ce1ddfe20
Justin Means <justincmeans@gmail.com>
2022-07-21 01:52:40 -0600

    Adds JWSGeoSpatial

---
+++ Sources/JBS/JBSCore/Models/Coordinates.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  
+//
+//  Created by Justin Means on 7/20/22.
+//
+
+import Foundation
+
+public struct Coordinates: Codable, Equatable, Hashable {
+	public init(longitude: Double, latitude: Double) {
+		self.longitude = longitude
+		self.latitude = latitude
+	}
+	
+	public init(arrayRepresentationLongLat: [Double]) throws {
+		guard arrayRepresentationLongLat.indices.contains(0) && arrayRepresentationLongLat.indices.contains(1) else { throw JBSError.custom("Longitude and Latitude not provided within array.") }
+		self.longitude = arrayRepresentationLongLat[0]
+		self.latitude = arrayRepresentationLongLat[1]
+	}
+	
+	public var longitude: Double
+	public var latitude: Double
+	
+	/// Standard for AWS SDKs
+	public var arrayRepresentationLongLat: [Double] {
+		[longitude, latitude]
+	}
+}
---
+++ Sources/JBS/JBSCore/Models/JBSError.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Justin Means on 7/20/22.
+//
+
+import Foundation
+
+public enum JBSError: Error {
+	case custom(_ text: String)
+}
