f182e95125379356f249b6bec7ec167472ae0704
Justin Means <justincmeans@gmail.com>
2023-03-02 21:51:43 -0700

    Adds OpenAI and HECO App Type

--- Sources/JBSDream/Core/OpenAI.swift
+++ Sources/JBSDream/Core/OpenAI.swift
@@ -12,7 +12,84 @@ public struct OpenAI {
 	public static var encoder: JSONEncoder {
 		let e = JSONEncoder()
 		e.dateEncodingStrategy = .iso8601
+		e.keyEncodingStrategy = .convertToSnakeCase
 		return e
+	}
+	
+	public struct ChatRequest: Codable {
+		public init(model: String = "gpt-3.5-turbo", messages: [OpenAI.ChatMessage], temperature: Double? = 1, topP: Double? = 1, n: Int? = 1, stream: Bool = false, stop: [String]? = nil, presencePenalty: Double? = 0, frequencyPenalty: Double? = 0, user: String? = nil) {
+			self.model = model
+			self.messages = messages
+			self.temperature = temperature
+			self.topP = topP
+			self.n = n
+			self.stream = stream
+			self.stop = stop
+			self.presencePenalty = presencePenalty
+			self.frequencyPenalty = frequencyPenalty
+			self.user = user
+		}
+		
+		public var model: String = "gpt-3.5-turbo"
+		public var messages: [ChatMessage]
+		/// What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+		/// We generally recommend altering this or top_p but not both.
+		public var temperature: Double? = 1
+		/// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+		/// We generally recommend altering this or temperature but not both.
+		public var topP: Double? = 1
+		/// How many chat completion choices to generate for each input message.
+		public var n: Int? = 1
+		public var stream: Bool = false
+		/// Up to 4 sequences where the API will stop generating further tokens.
+		public var stop: [String]?
+		public var presencePenalty: Double? = 0
+		public var frequencyPenalty: Double? = 0
+		/// A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.
+		public var user: String?
+		
+	}
+	
+	public struct ChatResponse: Codable {
+		public var id: String
+		public var object: String
+		public var created: Date
+		public var choices: [ChatChoice]
+		public var usage: ChatUsage
+	}
+	
+	public struct ChatUsage: Codable {
+		public var promptTokens: Int
+		public var completionTokens: Int
+		public var totalTokens: Int
+	}
+	
+	public struct ChatChoice: Codable {
+		public init(index: Int, message: OpenAI.ChatMessage, finishReason: String?) {
+			self.index = index
+			self.message = message
+			self.finishReason = finishReason
+		}
+		
+		public var index: Int
+		public var message: ChatMessage
+		public var finishReason: String?
+	}
+	
+	public struct ChatMessage: Codable {
+		public init(role: OpenAI.ChatMessage.Role, content: String) {
+			self.role = role
+			self.content = content
+		}
+		
+		public var role: Role
+		public var content: String
+		
+		public enum Role: String, RawRepresentable, Codable {
+			case system
+			case assistant
+			case user
+		}
 	}
 	
 	public struct CompletionRequest: Codable {
