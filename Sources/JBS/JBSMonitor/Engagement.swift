c7fd6659a4012c78257e5c74c42a18658ddd8ada
Justin Means <justincmeans@gmail.com>
2023-06-02 22:10:16 -0600

    Fix engagement tracking

--- Sources/JBS/JBSMonitor/Engagement.swift
+++ Sources/JBS/JBSMonitor/Engagement.swift
@@ -34,4 +34,50 @@ public extension Engagement {
         lhs.activity += rhs.activity
         lhs.pulses += rhs.pulses
     }
+    
+    static func +(lhs: Engagement, rhs: Engagement) -> Engagement {
+        let durationSeconds = lhs.durationSeconds + rhs.durationSeconds
+        let activity = lhs.activity + rhs.activity
+        let pulses = lhs.pulses + rhs.pulses
+        return Engagement(durationSeconds: durationSeconds, activity: activity, pulses: pulses)
+    }
+    
+    static func -(lhs: Engagement, rhs: Engagement) -> Engagement {
+        let durationSeconds = lhs.durationSeconds - rhs.durationSeconds
+        let activity = lhs.activity - rhs.activity
+        let pulses = lhs.pulses - rhs.pulses
+        return Engagement(durationSeconds: durationSeconds, activity: activity, pulses: pulses)
+    }
+    
+    static func *(lhs: Engagement, rhs: Double) -> Engagement {
+        let durationSeconds = lhs.durationSeconds * rhs
+        let activity = Int(Double(lhs.activity) * rhs)
+        let pulses = Int(Double(lhs.pulses) * rhs)
+        return Engagement(durationSeconds: durationSeconds, activity: activity, pulses: pulses)
+    }
+    
+    static func /(lhs: Engagement, rhs: Double) -> Engagement {
+        let durationSeconds = lhs.durationSeconds / rhs
+        let activity = Int(Double(lhs.activity) / rhs)
+        let pulses = Int(Double(lhs.pulses) / rhs)
+        return Engagement(durationSeconds: durationSeconds, activity: activity, pulses: pulses)
+    }
+    
+    static func -=(lhs: inout Engagement, rhs: Engagement) {
+        lhs.durationSeconds -= rhs.durationSeconds
+        lhs.activity -= rhs.activity
+        lhs.pulses -= rhs.pulses
+    }
+    
+    static func *=(lhs: inout Engagement, rhs: Double) {
+        lhs.durationSeconds *= rhs
+        lhs.activity = Int(Double(lhs.activity) * rhs)
+        lhs.pulses = Int(Double(lhs.pulses) * rhs)
+    }
+    
+    static func /=(lhs: inout Engagement, rhs: Double) {
+        lhs.durationSeconds /= rhs
+        lhs.activity = Int(Double(lhs.activity) / rhs)
+        lhs.pulses = Int(Double(lhs.pulses) / rhs)
+    }
 }
