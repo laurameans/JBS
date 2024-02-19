c40be3e6da03a2c1a1be16355e381d15ff207321
Justin Means <justincmeans@gmail.com>
2024-02-19 00:32:17 -0700

    Search

--- Sources/MeansBridge/JBSCore/Models/SearchResultRepresentable.swift
+++ Sources/MeansBridge/JBSCore/Models/SearchResultRepresentable.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-public protocol SearchResultRepresentable: Codable, Hashable, Equatable {
+public protocol SearchResultRepresentable: Codable, Hashable, Equatable, Identifiable {
 	var title: String { get }
 	var subtitle: String? { get }
 	var imageURL: String? { get }
-	
+    var id: UUID? { get }
 	
 }
