4b5197669a7122f7d5e123c6653621338df447f1
Justin Means <justincmeans@gmail.com>
2025-10-31 21:19:55 -0600

    macOS Distribution

--- Sources/JBS/JBSCore/Models/Binary.swift
+++ Sources/JBS/JBSCore/Models/Binary.swift
@@ -14,6 +14,22 @@ public struct Binary: Codable, Hashable, Equatable, Sendable {
     public var version: String
     public var releaseNotes: String
     public var releaseDescription: String
+    public var signature: String
+    public var sha256Hash: String
+    public var sizeBytes: Int64
+    public var bundleID: String
+    public var platformTag: String
+    public var downloadURL: String
+    public var isPublished: Bool
+    public var minMacOSVersion: String?
+    public var isCritical: Bool
+    public var channel: BinaryChannel
     public var createdDate: Date?
     public var updatedDate: Date?
+}
+
+public enum BinaryChannel: String, Codable, Sendable {
+    case production
+    case beta
+    case dev
 }
